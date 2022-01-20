b10edc4f84b5009b296f1fdd5bb171844c4f2f00
nvkvasiliy <vases74@gmail.com>
2022-01-20 18:00:19 +0500

    init

---
+++ index.js
@@ -0,0 +1,41 @@
+/**
+ * @typedef Person
+ * @type {object}
+ * @property {string} name - имя
+ * @property {Array<string>} interests - интересы
+ * @property {string} email - почта
+ * @property {{ startDate: Date, endDate: Date }} freeRange - диапазон для встречи
+ */
+
+/**
+ * @typedef Group
+ * @type {object}
+ * @property {() => Array<Person>} getAll - получить всех участников группы
+ * @property {(person: Person) => boolean} includePerson - добавить человека к списку участников
+ * @property {(email: string) => boolean} exludePerson - удалить человека из списка участников
+ */
+
+/**
+ * @param {string} interest - интерес группы
+ * @returns {Group} созданная группа
+ */
+export function createGroup(interest) {
+
+};
+
+/**
+ * @param {Group} group - группа людей
+ * @param {Date} meetingDate - дата встречи
+ * @returns {number} кол-во людей, готовых в переданную дату посетить встречу 
+ */
+export function findMeetingMembers(group, meetingDate) {
+
+};
+
+/**
+ * @param {Group} group - группа людей
+ * @returns {Date} дата, в которую могут собраться максимальное кол-во человек из группы
+ */
+export function findMeetingDateWithMaximumMembers(group) {
+
+};
