d69008c6dd43ff7ee8dd786d475a317189f5f6a7
Kisus228 <nik.babushkin.201408@gmail.com>
2022-03-02 03:55:53 +0500

    Решил все подзадачи

--- index.js
+++ index.js
@@ -20,24 +20,118 @@
  * @returns {Group} созданная группа
  */
 function createGroup(interest) {
+  let members = [];
 
-};
+  const getAll = () => members;
+
+  /**
+   * @param {Person} person
+   * @returns {boolean}
+   */
+  function includePerson(person) {
+    if (
+      members.some((x) => x.email === person.email) ||
+      !person.interests.some((x) => x === interest)
+    )
+      return false;
+
+    members.push(person);
+    return true;
+  }
+
+  function excludePerson(email) {
+    const memberIndex = members.findIndex((x) => x.email === email);
+
+    if (memberIndex === -1) return false;
+
+    members = members.filter((x) => x.email !== email);
+    return true;
+  }
+
+  return {
+    getAll: getAll,
+    includePerson: includePerson,
+    excludePerson: excludePerson,
+  };
+}
 
 /**
  * @param {Group} group - группа людей
  * @param {Date} meetingDate - дата встречи
- * @returns {number} кол-во людей, готовых в переданную дату посетить встречу 
+ * @returns {number} кол-во людей, готовых в переданную дату посетить встречу
  */
 function findMeetingMembers(group, meetingDate) {
+  if (
+    !(meetingDate instanceof Date) ||
+    isNaN(meetingDate.valueOf()) ||
+    !isGroup(group)
+  )
+    return 0;
 
-};
+  const peopleAmount = group
+    .getAll()
+    .filter((x) => isInRange(meetingDate, x.freeRange)).length;
+
+  return peopleAmount;
+}
+
+/**
+ *
+ * @param {Date} dateToCompare
+ * @param {{ startDate: Date, endDate: Date }} range
+ * @returns {boolean}
+ */
+function isInRange(dateToCompare, range) {
+  return dateToCompare >= range.startDate && dateToCompare <= range.endDate;
+}
+
+/**
+ *
+ * @param {Group} possibleGroup
+ * @returns {boolean}
+ */
+function isGroup(possibleGroup) {
+  return (
+    possibleGroup instanceof Object &&
+    possibleGroup.hasOwnProperty("getAll") &&
+    possibleGroup.hasOwnProperty("includePerson") &&
+    possibleGroup.hasOwnProperty("excludePerson")
+  );
+}
 
 /**
  * @param {Group} group - группа людей
  * @returns {Date} дата, в которую могут собраться максимальное кол-во человек из группы
  */
 function findMeetingDateWithMaximumMembers(group) {
+  if (!isGroup(group)) return null;
 
+  const members = group.getAll();
+
+  if (members.length === 0) return null;
+
+  if (members.length === 1) return members[0].freeRange.startDate;
+
+  const startDates = members.map((x) => x.freeRange.startDate);
+
+  let bestDate = new Date("01.01.2000");
+  let maximumPeople = 0;
+  startDates.forEach((date) => {
+    const peopleCount = members.filter((person) =>
+      isInRange(date, person.freeRange)
+    ).length;
+
+    if (peopleCount > maximumPeople) {
+      maximumPeople = peopleCount;
+      bestDate = date;
+    }
+  });
+
+  return bestDate;
+}
+
+module.exports = {
+  createGroup,
+  findMeetingMembers,
+  findMeetingDateWithMaximumMembers,
 };
-
-module.exports = { createGroup, findMeetingMembers, findMeetingDateWithMaximumMembers };
