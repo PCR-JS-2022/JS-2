696d763bff46d824306ac89cbe37a38dfddc7300
Dmtrii_Kh <khasimyanov@gmail.com>
2022-03-05 20:59:06 +0500

    Первый проход

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict'
 /**
  * @typedef Person
  * @type {object}
@@ -19,8 +20,75 @@
  * @param {string} interest - интерес группы
  * @returns {Group} созданная группа
  */
-function createGroup(interest) {
 
+const phoneList = [
+    {
+        name: 'Александра',
+        interests: ['games', 'computers'],
+        email: 'alexandra@rambler.ru',
+        freeRange: {
+            startDate: new Date('01.01.2020'),
+            endDate: new Date('03.10.2020'),
+        }
+    },
+    {
+        name: 'Василий',
+        interests: ['games'],
+        email: 'vasiliy@mail.ru',
+        freeRange: {
+            startDate: new Date('02.05.2020'),
+            endDate: new Date('02.25.2020'),
+        }
+    },
+    {
+        name: 'Роман',
+        email: 'roman@yandex.ru',
+        interests: ['javascript'],
+        freeRange: {
+            startDate: new Date('05.01.2020'),
+            endDate: new Date('06.10.2020'),
+        }
+    },
+    {
+        name: 'Егор',
+        email: 'egor@gmail.ru',
+        interests: ['computers', 'javascript'],
+        freeRange: {
+            startDate: new Date('03.01.2020'),
+            endDate: new Date('08.10.2020'),
+        }
+    },
+];
+
+
+function createGroup(interest) {
+    if (interest !== '' && typeof (interest) === 'string') {
+        return {
+            key: interest,
+            friends: [],
+            includePerson(friend) {
+                if (isFriendValid(friend) === true &&
+                    friend && friend.interests.includes(this.key) &&
+                    !this.friends.some((el) => isEqual(el, friend))) {
+                    this.friends.push(friend);
+                    return true
+                } else { return false };
+            },
+            excludePerson(mail) {
+                for (let i = 0; i < this.friends.length; i++) {
+                    if (this.friends[i].email === mail) {
+                        this.friends.splice(i, 1);
+                        return true
+                    } else {
+                        return false
+                    };
+                };
+            },
+            getAll() {
+                return this.friends
+            }
+        };
+    };
 };
 
 /**
@@ -28,16 +96,101 @@ function createGroup(interest) {
  * @param {Date} meetingDate - дата встречи
  * @returns {number} кол-во людей, готовых в переданную дату посетить встречу 
  */
+
 function findMeetingMembers(group, meetingDate) {
+    let stDay = 0;
+    let lastDay = 0;
+    if (group && meetingDate instanceof Date) {
+        group.friends.forEach(element => {
+            stDay += (element.freeRange.startDate <= meetingDate) ? 1 : 0;
+            lastDay += (element.freeRange.endDate >= meetingDate) ? 1 : 0;
+        });
+    };
+    return (stDay <= lastDay) ? stDay : lastDay
 
 };
+
+
 
 /**
  * @param {Group} group - группа людей
  * @returns {Date} дата, в которую могут собраться максимальное кол-во человек из группы
  */
-function findMeetingDateWithMaximumMembers(group) {
 
+function findMeetingDateWithMaximumMembers(group) {
+    const checkArr = group.getAll();
+    if (checkArr.length !== 0) {
+        const checkDates = [];
+        checkArr.forEach(el => {
+            checkDates.push(el.freeRange.startDate);
+            checkDates.push(el.freeRange.endDate);
+        });
+        checkDates.sort((a, b) => a >= b ? 1 : -1);
+        const finalCut = [];
+        checkDates.forEach(el => {
+            finalCut.push(
+                {
+                    date: el,
+                    friendsToMeet: findMeetingMembers(group, el)
+                }
+            );
+        });
+        return finalCut.sort((a, b) => a.friendsToMeet <= b.friendsToMeet ? 1 : -1)
+            .filter(el => el.friendsToMeet === finalCut[0].friendsToMeet)
+            .sort((a, b) => a.friendsToMeet <= b.friendsToMeet ? 1 : -1)[0].date;
+    } else {
+        return null
+    };
 };
+
+
+
+function isEqual(object1, object2) {
+    const props1 = Object.getOwnPropertyNames(object1);
+    const props2 = Object.getOwnPropertyNames(object2);
+
+    if (props1.length !== props2.length) {
+        return false;
+    } else {
+        for (let i = 0; i < props1.length; i++) {
+            const prop = props1[i];
+            const bothAreObjects = typeof (object1[prop]) === 'object' && typeof (object2[prop]) === 'object';
+
+            if ((!bothAreObjects && (object1[prop] !== object2[prop]))
+                || (bothAreObjects && !isEqual(object1[prop], object2[prop]))) {
+                return false;
+            };
+        };
+
+        return true;
+    };
+};
+
+// Это на всякий случай!!!
+function isFriendValid(el) {
+    if (el && typeof (el) === 'object' &&
+        el.name && typeof (el.name) === 'string' &&
+        el.interests && Array.isArray(el.interests) &&
+        el.interests.every((elem) => typeof (elem) === 'string') &&
+        el.email && typeof (el.email) === 'string' &&
+        el.freeRange && typeof (el.freeRange) === 'object' &&
+        el.freeRange.startDate && el.freeRange.startDate instanceof Date &&
+        el.freeRange.endDate && el.freeRange.endDate instanceof Date
+    ) {
+        return true
+    };
+}
+
+function arrayMin(arr) {
+    return arr.reduce((p, v) => {
+        return (p < v ? p : v);
+    });
+}
+
+function arrayMax(arr) {
+    return arr.reduce((p, v) => {
+        return (p > v ? p : v);
+    });
+}
 
 module.exports = { createGroup, findMeetingMembers, findMeetingDateWithMaximumMembers };
