6cbd70998ac7d3810baaf52b6ae5dda90e8559ba
Sharipov Aidar <Sharipow777@gmail.com>
2022-03-06 16:58:06 +0500

    first try

--- index.js
+++ index.js
@@ -20,7 +20,44 @@
  * @returns {Group} созданная группа
  */
 function createGroup(interest) {
+    const groupMembers = [];
+    const groupInterest = interest;
+    const group = {
+        getAll: () => groupMembers,
+        includePerson: (person) => {
+            if (!person 
+                || groupMembers.includes(person) 
+                || !person.interests.includes(groupInterest)) {
+                return false;
+            }
 
+            groupMembers.push(person);
+            return true;
+        },
+        excludePerson: (personEmail) => {
+            if (!personEmail) {
+                return false;
+            }
+
+            const groupMembersLength = groupMembers.length;
+            let personIndex = -1;
+            for (let i = 0;i < groupMembersLength; i++) {
+                if (groupMembers[i].email === personEmail) {
+                    personIndex = i;
+                    break;
+                }
+            }
+
+            if (personIndex === -1) {
+                return false;
+            }
+
+            groupMembers.splice(personIndex, 1);
+            return true;
+        }
+    }
+
+    return group;
 };
 
 /**
@@ -29,7 +66,26 @@ function createGroup(interest) {
  * @returns {number} кол-во людей, готовых в переданную дату посетить встречу 
  */
 function findMeetingMembers(group, meetingDate) {
+    if (!group || typeof group !== 'object' || typeof group.getAll !== 'function') {
+        return 0;
+    }
 
+    if (!meetingDate || !(meetingDate instanceof Date) || isNaN(meetingDate.valueOf())) {
+        return 0;
+    }
+
+    let result = 0;
+    group.getAll().forEach(person => {
+        if (!person.freeRange) {
+            return;
+        }
+
+        if (person.freeRange.startDate <= meetingDate && person.freeRange.endDate >= meetingDate) {
+            result++;
+        }
+    });
+
+    return result;
 };
 
 /**
@@ -37,7 +93,31 @@ function findMeetingMembers(group, meetingDate) {
  * @returns {Date} дата, в которую могут собраться максимальное кол-во человек из группы
  */
 function findMeetingDateWithMaximumMembers(group) {
+    if (!group || typeof group !== 'object' || typeof group.getAll !== 'function') {
+        return null;
+    }
 
+    const members = group.getAll();
+    let result = null;
+    let maxMembers = 0;
+
+    members.forEach(member => {
+        const memberStartDate = member.freeRange.startDate;
+        const membersInRange = members.filter(m => {
+            return m.freeRange.startDate <= memberStartDate && memberStartDate <= m.freeRange.endDate;
+        });
+
+        if (membersInRange.length > maxMembers) {
+            result = memberStartDate;
+            maxMembers = membersInRange.length;
+        }
+        
+        if (membersInRange.length === maxMembers && memberStartDate < result) {
+            result = memberStartDate;
+        }
+    });
+
+    return result;
 };
 
 module.exports = { createGroup, findMeetingMembers, findMeetingDateWithMaximumMembers };
