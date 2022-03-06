c3a0a5abc72a81731382665d4a370ea625c80a8c
murrnyaka <murrrnyaka@mail.ru>
2022-03-06 13:31:27 +0500

    первая попытка

--- index.js
+++ index.js
@@ -19,9 +19,84 @@
  * @param {string} interest - интерес группы
  * @returns {Group} созданная группа
  */
-function createGroup(interest) {
 
+ const phoneList = [
+    {
+      name: 'Александра',
+      interests: ['games', 'computers'],
+      email: 'alexandra@rambler.ru',
+      freeRange: {
+        startDate: new Date('01.01.2020'),
+        endDate: new Date('03.10.2020'),
+      }
+    },
+    {
+      name: 'Василий',
+      interests: ['games'],
+      email: 'vasiliy@mail.ru',
+      freeRange: {
+        startDate: new Date('02.05.2020'),
+        endDate: new Date('02.25.2020'),
+      }
+    },
+    {
+      name: 'Роман',
+      email: 'roman@yandex.ru',
+      interests: ['javascript'],
+      freeRange: {
+        startDate: new Date('05.01.2020'),
+        endDate: new Date('06.10.2020'),
+      }
+    },
+    {
+      name: 'Егор',
+      email: 'egor@gmail.ru',
+      interests: ['computers', 'javascript'],
+      freeRange: {
+        startDate: new Date('03.01.2020'),
+        endDate: new Date('08.10.2020'),
+      }
+    },
+  ];
+
+function createGroup(interest) {
+    const friendsGroup = [];
+    return {
+        getAll() {
+            return friendsGroup;
+        },
+        includePerson(friend) {
+            if(friend.interests === undefined) return false;
+            if(friend.interests.includes(interest)) {
+                if(friendsGroup.indexOf(friend) === -1) {
+                    friendsGroup.push(friend);
+                }
+                return true;
+            } else {
+                return false;
+            }
+        },
+        excludePerson(email) {
+            let result = false;
+            phoneList.forEach(friend => {
+                if(friend.email === email) {
+                    friendsGroup.splice(friendsGroup.indexOf(friend), 1);
+                    if(friendsGroup.indexOf(friend) === -1) {
+                        result = true;
+                    }
+                }
+            });
+            return result;
+        }
+    }
 };
+// const javaScriptGroup = createGroup("javascript");
+// console.log(javaScriptGroup.getAll())
+// console.log(javaScriptGroup.includePerson(phoneList[3]));
+// console.log(javaScriptGroup.includePerson(phoneList[2]));
+// console.log(javaScriptGroup.excludePerson('roman@yandex.ru'))
+// console.log(javaScriptGroup.excludePerson("'vasiliy@mail.ru'"))
+// console.log(javaScriptGroup.getAll())
 
 /**
  * @param {Group} group - группа людей
@@ -29,15 +104,59 @@ function createGroup(interest) {
  * @returns {number} кол-во людей, готовых в переданную дату посетить встречу 
  */
 function findMeetingMembers(group, meetingDate) {
-
+    if(!(group.getAll || meetingDate instanceof Date)) return 0;
+    const friendsGroup = group.getAll();
+    meetingDate = meetingDate.getTime();
+    return friendsGroup.reduce((count, current) => {
+        const startDate = current.freeRange.startDate.getTime();
+        const endDate = current.freeRange.endDate.getTime();
+        if(meetingDate >= startDate && meetingDate <= endDate) {
+            count++;
+        }
+        return count;
+    }, 0)
 };
+// const javaScriptGroup = createGroup('javascript');
+// javaScriptGroup.includePerson(phoneList[2]); // true
+// javaScriptGroup.includePerson(phoneList[3]); // true
+
+// console.log(findMeetingMembers(javaScriptGroup, new Date('10.10.2020'))); // 0
+// console.log(findMeetingMembers(javaScriptGroup, new Date('06.10.2020'))); // 2
 
 /**
  * @param {Group} group - группа людей
  * @returns {Date} дата, в которую могут собраться максимальное кол-во человек из группы
  */
-function findMeetingDateWithMaximumMembers(group) {
 
+function findIndexMax(arr) {
+    let max = arr[0];
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] > max) max = arr[i];
+    }
+    return arr.indexOf(max);
 };
+
+function findMeetingDateWithMaximumMembers(group) {
+    if(!group.getAll) return null;
+
+    const friendsGroup = group.getAll();
+
+    const dates = friendsGroup.reduce((res, current) => { //собрали все даты
+        res.push(current.freeRange.startDate);
+        res.push(current.freeRange.endDate);
+        return res;
+    }, []);
+    const sortedDates = dates.sort((a, b) => {
+        return a - b;
+    })
+    const counts = sortedDates.map(i => findMeetingMembers(group, i));
+    const maxCountIndex = findIndexMax(counts);
+    return sortedDates[maxCountIndex];
+};
+
+// const javaScriptGroup = createGroup('games');
+// javaScriptGroup.includePerson(phoneList[0]); // true
+// javaScriptGroup.includePerson(phoneList[1]); // true
+// console.log(findMeetingDateWithMaximumMembers(javaScriptGroup));
 
 module.exports = { createGroup, findMeetingMembers, findMeetingDateWithMaximumMembers };
