7c53f46675019057216ac3392df6b4e6b41e69d3
Михалев Кирилл <mihalov.k@mail.ru>
2022-03-07 04:27:26 +0500

    Первая попытка

--- index.js
+++ index.js
@@ -20,8 +20,34 @@
  * @returns {Group} созданная группа
  */
 function createGroup(interest) {
+    return {
+        interest: interest,
+        personArray : [],
 
-};
+        getAll() {
+            return this.personArray
+        },
+
+        includePerson(Person) {
+            if (!Array.isArray(Person.interests) || typeof Person.name !== "string"
+                || typeof Person.email !== "string" || !Person.freeRange.startDate instanceof Date
+                || !Person.freeRange.endDate instanceof Date) return false
+            if (Person.interests.find(interest => interest === this.interest)
+                && !this.personArray.find(elem => elem === Person)) {
+                this.personArray.push(Person)
+                return true
+            } else return false
+        },
+
+        excludePerson(email) {
+            const found = this.personArray.find(elem => elem.email === email && typeof email === "string");
+            if (found) {
+                this.personArray.splice(this.personArray.indexOf(found), 1);
+                return true
+            } else return false;
+        }
+    }
+}
 
 /**
  * @param {Group} group - группа людей
@@ -29,15 +55,36 @@ function createGroup(interest) {
  * @returns {number} кол-во людей, готовых в переданную дату посетить встречу 
  */
 function findMeetingMembers(group, meetingDate) {
-
-};
+    if (group.hasOwnProperty('getAll') && meetingDate instanceof Date)
+        return group.personArray.filter(elem => elem.freeRange.startDate <= meetingDate &&
+            elem.freeRange.endDate >= meetingDate).length
+    else return 0
+}
 
 /**
  * @param {Group} group - группа людей
  * @returns {Date} дата, в которую могут собраться максимальное кол-во человек из группы
  */
 function findMeetingDateWithMaximumMembers(group) {
-
-};
+    let resultDate = null;
+    if (!group.hasOwnProperty('getAll'))
+        return resultDate;
+    const startArr = [];
+    const endArr = [];
+    group.personArray.forEach(elem => {
+        startArr.push(elem.freeRange.startDate)
+        endArr.push(elem.freeRange.endDate)
+    })
+    const min = startArr.sort((a,b) => a-b)[0]
+    const max = endArr.sort((a,b) => a-b)[endArr.length-1];
+    let max_length = 1;
+    for (let date = min;date < max;date.setDate(date.getDate() + 1)) {
+        if (findMeetingMembers(group,date) > max_length) {
+            max_length = findMeetingMembers(group,date);
+            resultDate = new Date(date);
+        }
+    }
+    return resultDate;
+}
 
 module.exports = { createGroup, findMeetingMembers, findMeetingDateWithMaximumMembers };
