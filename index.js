9cd027161ba6c19139cdea38e7e0f18c3dcb5c1e
apterousteen <ayzhan312@live.ru>
2022-03-07 23:04:00 +0500

    2 task

--- index.js
+++ index.js
@@ -51,70 +51,44 @@ function createGroup(interest) {
     }
 };
 
-// const phoneList = [
-//     {
-//         name: 'Александра',
-//         interests: ['games', 'computers'],
-//         email: 'alexandra@rambler.ru',
-//         freeRange: {
-//             startDate: new Date('01.01.2020'),
-//             endDate: new Date('03.10.2020'),
-//         }
-//     },
-//     {
-//         name: 'Василий',
-//         interests: ['games'],
-//         email: 'vasiliy@mail.ru',
-//         freeRange: {
-//             startDate: new Date('02.05.2020'),
-//             endDate: new Date('02.25.2020'),
-//         }
-//     },
-//     {
-//         name: 'Роман',
-//         email: 'roman@yandex.ru',
-//         interests: ['javascript'],
-//         freeRange: {
-//             startDate: new Date('05.01.2020'),
-//             endDate: new Date('06.10.2020'),
-//         }
-//     },
-//     {
-//         name: 'Егор',
-//         email: 'egor@gmail.ru',
-//         interests: ['computers', 'javascript'],
-//         freeRange: {
-//             startDate: new Date('03.01.2020'),
-//             endDate: new Date('08.10.2020'),
-//         }
-//     },
-// ];
-
-
-// const javaScriptGroup = createGroup('javascript');
-// console.log(JSON.stringify(javaScriptGroup.includePerson(phoneList[2])));
-// console.log(JSON.stringify(javaScriptGroup.includePerson(phoneList[0])));
-// console.log(JSON.stringify(javaScriptGroup.includePerson(phoneList[3])));
-// console.log('   ')
-// console.log(JSON.stringify(javaScriptGroup.excludePerson('vasiliy@mail.ru')));
-// console.log(JSON.stringify(javaScriptGroup.excludePerson('roman@yandex.ru')));
-// console.log(JSON.stringify(javaScriptGroup.getAll()));
-
-/*
-  [
+const phoneList = [
     {
-      name: 'Егор',
-      interests: ['computers', 'javascript'],
-      email: 'egor@gmail.ru',
-      freeRange: {
-        startDate: new Date('03.01.2020'),
-        endDate: new Date('08.10.2020'),
-      }
+        name: 'Александра',
+        interests: ['games', 'computers'],
+        email: 'alexandra@rambler.ru',
+        freeRange: {
+            startDate: new Date('01.01.2020'),
+            endDate: new Date('03.10.2020'),
+        }
     },
-  ]
-*/
-
-
+    {
+        name: 'Василий',
+        interests: ['games'],
+        email: 'vasiliy@mail.ru',
+        freeRange: {
+            startDate: new Date('02.05.2020'),
+            endDate: new Date('02.25.2020'),
+        }
+    },
+    {
+        name: 'Роман',
+        email: 'roman@yandex.ru',
+        interests: ['javascript'],
+        freeRange: {
+            startDate: new Date('05.01.2020'),
+            endDate: new Date('06.10.2020'),
+        }
+    },
+    {
+        name: 'Егор',
+        email: 'egor@gmail.ru',
+        interests: ['computers', 'javascript'],
+        freeRange: {
+            startDate: new Date('03.01.2020'),
+            endDate: new Date('08.10.2020'),
+        }
+    },
+];
 
 /**
  * @param {Group} group - группа людей
@@ -122,8 +96,22 @@ function createGroup(interest) {
  * @returns {number} кол-во людей, готовых в переданную дату посетить встречу 
  */
 function findMeetingMembers(group, meetingDate) {
+    let count = 0;
 
+    group.getAll().forEach(person => {
+        ((person.freeRange.startDate <= meetingDate) && (meetingDate <= person.freeRange.endDate)) ? count++ : count;
+    })
+
+    return count;
 };
+
+// const javaScriptGroup = createGroup('javascript');
+// console.log(JSON.stringify(javaScriptGroup.includePerson(phoneList[2]))); // true
+// console.log(JSON.stringify(javaScriptGroup.includePerson(phoneList[3]))); // true
+// console.log('   ')
+// console.log(JSON.stringify(findMeetingMembers(javaScriptGroup, new Date('10.10.2020')))); // 0
+// console.log(JSON.stringify(findMeetingMembers(javaScriptGroup, new Date('06.10.2020')))); // 2
+
 
 /**
  * @param {Group} group - группа людей
