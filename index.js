512a8c4ee7b8e1e763bc2fd03e445229eba4c0ab
NikitaKirin <kirinnikita1406@gmail.com>
2022-03-07 23:06:36 +0500

    refactoring

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 /**
+ * Check, is variable object with type "Group" or not
+ * @param {Group} group
+ * @returns {boolean}
+ */
+function isGroup(group) {
+    return (typeof group === 'object')
+        && (group.hasOwnProperty('getAll'))
+        && (group.hasOwnProperty('includePerson'))
+        && (group.hasOwnProperty('excludePerson'));
+}
+
+/**
+ * Check, is variable object with type "Person" or not
+ * @param {Person} person
+ * @returns {boolean}
+ */
+function isPerson(person) {
+    return (typeof person === 'object')
+        && (person.hasOwnProperty('name') && typeof person.name === 'string')
+        && (person.hasOwnProperty('interests') && Array.isArray(person.interests))
+        && (person.hasOwnProperty('email') && typeof person.email === 'string')
+        && (person.hasOwnProperty('freeRange') && typeof person.freeRange === 'object');
+}
+
+/**
  * @typedef Person
  * @type {object}
  * @property {string} name - имя
@@ -28,8 +53,10 @@ function createGroup(interest) {
         },
         includePerson: function (person) {
             if (!isPerson(person)) return false;
-            if ((person.interests.find((interest) => interest === this.interest) !== undefined)
-                && this.friends.find((friend) => friend.email === person.email) === undefined) {
+            const findInterest = person.interests.includes(this.interest);
+            //const findEmail = this.friends.find((friend) => friend.email === person.email);
+            const findEmail = this.friends.email.includes(person.email);
+            if (findInterest && !findEmail) {
                 this.friends.push(person);
                 return true;
             }
@@ -37,12 +64,9 @@ function createGroup(interest) {
         },
         excludePerson: function (email) {
             if (typeof email !== 'string') return false;
-            let person = this.friends.findIndex((friend) => friend.email === email);
-            if (person !== -1) {
-                this.friends.splice(person, 1);
-                return true;
-            }
-            return false;
+            const tempFriendsCount = this.getAll().length;
+            this.friends = this.friends.filter(person => person.email !== email);
+            return tempFriendsCount !== this.friends.length;
         }
     };
 };
@@ -56,10 +80,11 @@ function findMeetingMembers(group, meetingDate) {
     if (!isGroup(group) || !meetingDate instanceof Date || isNaN(+meetingDate))
         return 0;
     let personCount = 0;
-    for (let person of group.getAll()) {
-        if (+meetingDate >= +person.freeRange.startDate && +meetingDate <= +person.freeRange.endDate)
+    group.getAll().forEach((person) => {
+        if (Number(meetingDate) >= Number(person.freeRange.startDate) && Number(meetingDate) <= Number(person.freeRange.endDate)) {
             personCount++;
-    }
+        }
+    })
     return personCount;
 };
 
@@ -70,42 +95,18 @@ function findMeetingMembers(group, meetingDate) {
 function findMeetingDateWithMaximumMembers(group) {
     if (!isGroup(group) || group.getAll().length === 0)
         return null;
-    let dateArr = group.getAll().map((person) => [person.freeRange.startDate, 0]);
-    for (let date of dateArr) {
-        for (let person of group.getAll()) {
-            date[1] = findMeetingMembers(group, date[0]);
-        }
-    }
-    dateArr.sort((a, b) => b[1] - a[1]);
-    if (dateArr[0][1] === 0)
-        return null;
-    return dateArr[0][0];
+    const dateArr = group.getAll().map((person) => [person.freeRange.startDate, 0]);
+    const resultArr = dateArr.map((date) => [date[0], findMeetingMembers(group, date[0])]);
+    /*    for (let date of dateArr) {
+            for (let person of group.getAll()) {
+                date[1] = findMeetingMembers(group, date[0]);
+            }
+        }*/
+    resultArr.sort((a, b) => b[1] - a[1]);
+    const answerDate = resultArr[0];
+    if (answerDate[1] === 0) return null;
+    return answerDate[0];
 };
-
-/**
- * Check, is variable object with type "Group" or not
- * @param {Group} group
- * @returns {boolean}
- */
-function isGroup(group) {
-    return (typeof group === 'object')
-        && (group.hasOwnProperty('getAll'))
-        && (group.hasOwnProperty('includePerson'))
-        && (group.hasOwnProperty('excludePerson'));
-}
-
-/**
- * Check, is variable object with type "Person" or not
- * @param {Person} person
- * @returns {boolean}
- */
-function isPerson(person) {
-    return (typeof person === 'object')
-        && (person.hasOwnProperty('name') && typeof person.name === 'string')
-        && (person.hasOwnProperty('interests') && Array.isArray(person.interests))
-        && (person.hasOwnProperty('email') && typeof person.email === 'string')
-        && (person.hasOwnProperty('freeRange') && typeof person.freeRange === 'object');
-}
 
 module.exports = {
     createGroup,
