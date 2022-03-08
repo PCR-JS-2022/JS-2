ec2b3674b46ed2a706b67a1f3e631e793fa67412
Евгений Куликов <ekueku2001@icloud.com>
2022-03-08 16:50:22 +0500

    ...

--- index.js
+++ index.js
@@ -1,47 +1,3 @@
-const phoneList = [
-	{
-		name: 'Александра',
-		interests: ['games', 'computers'],
-		email: 'alexandra@rambler.ru',
-		freeRange: {
-			startDate: new Date('01.01.2020'),
-			endDate: new Date('03.10.2020'),
-		}
-	},
-	{
-		name: 'Василий',
-		interests: ['games'],
-		email: 'vasiliy@mail.ru',
-		freeRange: {
-			startDate: new Date('02.05.2020'),
-			endDate: new Date('02.25.2020'),
-		}
-	},
-	{
-		name: 'Роман',
-		email: 'roman@yandex.ru',
-		interests: ['javascript'],
-		freeRange: {
-			startDate: new Date('05.01.2020'),
-			endDate: new Date('06.10.2020'),
-		}
-	},
-	{
-		name: 'Егор',
-		email: 'egor@gmail.ru',
-		interests: ['computers', 'javascript'],
-		freeRange: {
-			startDate: new Date('03.01.2020'),
-			endDate: new Date('08.10.2020'),
-		}
-	},
-];
-
-var g = createGroup('games');
-var a = g.includePerson(phoneList[0]);
-var b = g.includePerson(phoneList[0]);
-console.log(a, b);
-
 /**
  * @typedef Person
  * @type {object}
@@ -67,7 +23,7 @@ function createGroup(interest) {
 	let persons = [];
 	return {
 		getAll() {
-			return persons
+			return persons;
 		},
 		includePerson(person) {
 			if (person?.interests?.includes(interest) && !persons.includes(person)) {
