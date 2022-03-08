637420b710e4f98d875daa5bcc74694f4f905c0f
danichka <daniika.ella@gmail.com>
2022-03-09 02:31:39 +0500

    Ласт трай

--- index.js
+++ index.js
@@ -1,56 +1,73 @@
 function createGroup(interest) {
   let members = [];
   const getAll = () => members;
-  
+
   function onMember(person) {
-    if (person.interests?.includes(interest) && 
-    !people.map((p) => p.email).includes(person.email)){
-        people.push(person);
-        return true;
-    }
-    return false;
+     if (
+       person.interests === undefined ||
+       members.some((x) => x.email === person.email) ||
+       !person.interests.some((x) => x === interest)
+     )
+      return false;
+    members.push(person);
+    return true;
   }
-
   function offMember(email) {
-    const index = people.findIndex((p) => p.email === email);
-        if (index != -1){
-            people.splice(index);
-            return true;
-        }
-        return false;
-    }
-  return { getAll, onMember, offMember }
-};
-
-function testRange(tempDate, range) {
-  return tempDate >= range.startDate && tempDate <= range.endDate;
+    const memberIndex = members.findIndex((x) => x.email === email);
+    if (memberIndex === -1) return false;
+    members = members.filter((x) => x.email !== email);
+    return true;
+  }
+  return { getAll, onMember, offMember, };
 }
 
 function findMeetingMembers(group, meetingDate) {
-  return (group.getAll && group.getAll()) && 
-  (meetingDate instanceof Date) ? group
+  if (
+    !(meetingDate instanceof Date) ||
+    isNaN(meetingDate.valueOf()) ||
+    !testGroup(group)
+  )
+    return 0;
+  const peopleAmount = group
     .getAll()
-    .filter((p) => testRange(meetingDate, p.freeRange))
-    .length : 0;
-};
+    .filter((x) => testRange(meetingDate, x.freeRange)).length;
+  return peopleAmount;
+}
+
+function testRange(dateToCompare, range) {
+  return dateToCompare >= range.startDate && dateToCompare <= range.endDate;
+}
+
+function testGroup(possibleGroup) {
+  return (
+    possibleGroup instanceof Object &&
+    Object.prototype.hasOwnProperty.call(possibleGroup, "getAll") &&
+    Object.prototype.hasOwnProperty.call(possibleGroup, "onMember") &&
+    Object.prototype.hasOwnProperty.call(possibleGroup, "offMembe")
+  );
+}
 
 function findMeetingDateWithMaximumMembers(group) {
-  const members = group.getAll() && group.getAll();
-  if (!members) return null;
+  if (!testGroup(group)) return null;
+  const members = group.getAll();
+  if (members.length === 0) return null;
   if (members.length === 1) return members[0].freeRange.startDate;
-
-  const dates = members.map((x) => x.freeRange.startDate);
-  let resultDate= startDates.sort((a, b) => a - b)[0];
-  let maxPeople = 0;
-  dates.forEach((date) => {const counter = members
-    .filter((person) => isInRange(date, person.freeRange))
-    .length;
-    
-    if (counter > maxPeople) {
-        maxPeople = counter;
-        resultDate = date;
-    } });
+  const startDates = members.map((x) => x.freeRange.startDate);
+  let resultDate = new Date("01.01.2000");
+  let maximumPeople = 0;
+  startDates.forEach((date) => {
+    const peopleCount = members.filter((person) =>
+      isInRange(date, person.freeRange)
+    ).length;
+    if (peopleCount > maximumPeople) {
+      maximumPeople = peopleCount;
+      resultDate = date;
+    }
+  });
   return resultDate;
+}
+module.exports = {
+  createGroup,
+  findMeetingMembers,
+  findMeetingDateWithMaximumMembers,
 };
-
-module.exports = { createGroup, findMeetingMembers, findMeetingDateWithMaximumMembers };
