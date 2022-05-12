0892214d267d05ca391ef2e450dfbd7cade65b84
LenaVas <vasyasina.lena11@gmail.com>
2022-05-12 17:51:31 +0500

    Fix

--- index.js
+++ index.js
@@ -47,9 +47,9 @@ function createGroup(interest) {
  * @returns {number} кол-во людей, готовых в переданную дату посетить встречу 
  */
 function findMeetingMembers(group, meetingDate) {
-    const allPerson = group?.getAll();
-    if (!allPerson) return 0;
-    return allPerson.map(person => person.freeRange).reduce((count, freeRange) =>
+    if (!meetingDate instanceof Date || !group.getAll) return 0;
+    return group.getAll().map(person => person.freeRange)
+        .reduce((count, freeRange) =>
             freeRange.startDate <= meetingDate && freeRange.endDate >= meetingDate ? count + 1 : count
         , 0)
 };
@@ -59,11 +59,10 @@ function findMeetingMembers(group, meetingDate) {
  * @returns {Date} дата, в которую могут собраться максимальное кол-во человек из группы
  */
 function findMeetingDateWithMaximumMembers(group) {
-    const allPerson = group?.getAll();
-    if (!allPerson) {
+    if (!group.getAll) {
         return null;
     }
-    return allPerson
+    return group.getAll()
         .map(person => person.freeRange)
         .reduce((count, freeRange) => {
             const currentCount = findMeetingMembers(group, freeRange.startDate);
